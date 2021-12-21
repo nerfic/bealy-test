f6c3f4a135328bc80a2c88684ca58f1f9604220e
Allan <jdll.allan@gmail.com>
2021-12-21 15:24:38 +0100

    Add try and catch

--- server/middleware/findRoomByUuid.js
+++ server/middleware/findRoomByUuid.js
@@ -1,19 +1,24 @@
 const Room = require('../models/rooms')
 
 async function findRoomByUuid(req, res, next) {
-    const uuid = await Room.findOne({
-        where: {
-            uuid: req.body.room_uuid
-        }
-    })
-    if (uuid) {
-        req.room = uuid
-        return next();
-    } else {
-        res.status(404).json({
-            status: 404,
-            message: "Room not exist"
+    try {
+        const uuid = await Room.findOne({
+            where: {
+                uuid: req.body.room_uuid
+            }
         })
+        if (uuid) {
+            req.room = uuid
+            return next();
+        } else {
+            res.status(404).json({
+                status: 404,
+                message: "Room not exist"
+            })
+        }
+    } catch (err) {
+        console.log(err)
+        res.send(err)
     }
 }
 

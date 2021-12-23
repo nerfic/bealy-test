ac552f87a1b3023cf62f4e87c524d8c9e9fbadc4
Allan <jdll.allan@gmail.com>
2021-12-23 10:38:51 +0100

    Fix component route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './views/Login';
-import LoginCopy from './views/LoginCopy'
 import Home from './views/Home';
 import MyRoom from './views/MyRoom';
 import FileTransfer from './views/FileTransfer';
@@ -21,7 +20,7 @@ function App() {
           <Route exact path="/transfert/:uuid" element={<FileTransfer />} />
           <Route exact path="/logout" element={<Logout />} />
         </Route>
-        <Route exact path="/login" element={<LoginCopy />} />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="*" element={<NotFound />} />
       </Routes>
